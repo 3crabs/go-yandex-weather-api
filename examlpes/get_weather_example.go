dd516218da50dd58d08e102db1380f96f6993e6a
FedorovVladimir <vladimir.fodorow@gmail.com>
2021-09-19 18:14:59 +0700

    fix data format

--- examlpes/get_weather_example.go
+++ examlpes/get_weather_example.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 	yandexWeatherApiKey := "YOUR_YANDEX_WEATHER_API_KEY"
-	w, _ := weather.GetWeather(yandexWeatherApiKey, 53.366, 83.5)
+	w, _ := weather.GetWeather(yandexWeatherApiKey, 53.3, 83.5)
 	fmt.Println(w)
 }
