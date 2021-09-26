f93c6cb77abe9b75940c5f3ab0eb2c7271f85f33
Irina Turenko <turenko7irina@mail.ru>
2021-09-26 23:04:05 +0300

    добавлена функция GetCondition в файл main.go + вызов этой функции в файле mini_weather_report.go

--- examlpes/mini_weather_report.go
+++ examlpes/mini_weather_report.go
@@ -9,7 +9,8 @@ import (
 func main() {
 	yandexWeatherApiKey := "YOUR_YANDEX_WEATHER_API_KEY"
 	w, _ := weather.GetWeatherWithCache(yandexWeatherApiKey, 53.3, 83.5, time.Hour)
-	fmt.Printf("Сегодня %d°C\n", w.Fact.Temp)
+	fmt.Printf("Сегодня %s\n", w.Fact.GetCondition())
+	fmt.Printf("Температура %d°C\n", w.Fact.Temp)
 	fmt.Printf("Ощущается как %d°C\n", w.Fact.FeelsLike)
-	fmt.Printf("Порывы ветра до %dм/с\n", w.Fact.WindGust)
+	fmt.Printf("Порывы ветра до %d м/с\n", w.Fact.WindGust)
 }
